6a41ca78f6c386a36e9f40ebdf583ad9b50ea1c6
lightniing1 <dan-@outlook.com>
2023-10-06 17:17:59 -0300

    front end

--- svelte-todo-app/vite.config.ts
+++ svelte-todo-app/vite.config.ts
@@ -4,4 +4,13 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+   host: true,
+   port: 8000, // This is the port which we will use in docker
+   // Thanks @sergiomoura for the window fix
+   // add the next lines if you're using windows and hot reload doesn't work
+    watch: {
+      usePolling: true
+    }
+ }
 })
